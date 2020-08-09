57d73cdb54a3196ff33316bd2fbd867b9a304091
Carrie Mathieu <carrieburks@gmail.com>
2020-08-09 18:11:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,3 +3,23 @@ def my_each(array) # put argument(s) here
   while i < array.length
   # code here
 end
+
+def hello_t(array)
+  if block_given?
+    i = 0 # counter variable
+    while i < array.length 
+      yield array[i]
+      i += 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
+
+# call your method here!
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#  if name.start_with?("T")
+ #   puts "Hi, #{name}"
+#  end
+#end
